84ad12556373af66bfa984c1a896542964c58fe6
Qianxi Kong <qianxikong@gmail.com>
2024-08-30 10:06:20 -0400

    lab1

--- analyze.py
+++ analyze.py
@@ -2,9 +2,10 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+endpoint = os.getenv('Endpoint')
+key = os.getenv('API_KEY')
 
 credentials = CognitiveServicesCredentials(key)
 
